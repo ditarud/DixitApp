87edf82a2ca67cbf681607e8730e6048123e23da
ditarud <ditarud@miuandes.cl>
2019-09-24 12:14:35 -0300

    Change name app , and routing views

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,11 @@ import { UserI } from '../models/user.interface';
 import { UserService } from '../services/user.service';
 import { LocalNotifications, ELocalNotificationTriggerUnit } from '@ionic-native/local-notifications/ngx';
 import { Platform } from '@ionic/angular';
+import { AuthenticateService } from '../services/authentication.service';
+import { NavController, ModalController } from '@ionic/angular';
+
+
+
 
 @Component({
   selector: 'app-home',
@@ -10,8 +15,11 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
+  userEmail: string;
   users: UserI[];
-  constructor(private plt: Platform, private userService:UserService,private localNotifications: LocalNotifications) {
+  constructor(private plt: Platform, private userService:UserService,private localNotifications: LocalNotifications,
+    private navCtrl: NavController, private authService: AuthenticateService) {
+
     this.plt.ready().then(() => {
       this.localNotifications.on('click').subscribe(res => {
         console.log('Click:', res);
@@ -28,9 +36,26 @@ export class HomePage implements OnInit {
 
   ngOnInit(){
     this.userService.getUsers().subscribe(res =>  this.users = res);
+    
   
   }
 
+  
+  logout(){
+    this.authService.logoutUser()
+    .then(res => {
+      console.log(res);
+      this.navCtrl.navigateBack('');
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+  
+  goDashboard(){
+    this.navCtrl.navigateForward('/dashboard');
+
+  }
   scheduleNotification(){
     this.localNotifications.schedule({
       id: 1,
